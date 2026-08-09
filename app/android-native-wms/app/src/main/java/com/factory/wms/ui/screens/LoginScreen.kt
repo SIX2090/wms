e53f7bbbbb8fd29e245e5e7cff0d79c2ba17a662
SIX2090 <SIX2090@users.noreply.github.com>
2026-08-09 06:39:29 +0000

    fix(android): 默认服务器地址改为 https://gd2026.top
    nginx 只监听 443 (HTTPS)，http://gd2026.top 打到 80 端口返回 405 Method Not Allowed。

--- app/android-native-wms/app/src/main/java/com/factory/wms/ui/screens/LoginScreen.kt
+++ app/android-native-wms/app/src/main/java/com/factory/wms/ui/screens/LoginScreen.kt
@@ -40,7 +40,7 @@ fun LoginScreen(
     val uiState by viewModel.uiState.collectAsState()
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var baseUrl by remember { mutableStateOf(uiState.baseUrl.ifEmpty { "http://gd2026.top" }) }
+    var baseUrl by remember { mutableStateOf(uiState.baseUrl.ifEmpty { "https://gd2026.top" }) }
     var showPassword by remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
     val snackbarHostState = remember { SnackbarHostState() }
